ac02c12513e5c90bcaffd1e046d4fcdef8888eec
santhosh2362003 <santhoshraj2362003@gmail.com>
2026-04-22 14:29:30 +0530

    Create script.js

---
+++ project/frontend/script.js
@@ -0,0 +1,29 @@
+const API = "http://localhost:5000";
+
+async function register() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch(`${API}/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+    alert(data.message);
+}
+
+async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch(`${API}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+    alert(data.message);
+}
